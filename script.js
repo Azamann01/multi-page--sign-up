311379078121c0ceea0d22b1a9601a93be0625cc
Tope Wilson <adeniyitopewilson@gmail.com>
2025-02-01 17:41:14 +0000

    adding functions with javascripts to each pages

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 document.addEventListener('DOMContentLoaded', function(){
+    // targeting each form containers 
     const signUpContainer = document.getElementById('sign-up-container');
+    const signUpCon = document.getElementById('sign-up-con');
+    const signUpContain = document.getElementById('sign-up-contain');
+    // targeting buttons 
     const nextPageButton = document.getElementById('btn-next');
-    const nameDetails = document.getElementById('name-details-container');
     const prevPageButton = document.getElementById('btn-prev-sec');
-    const nextDetailsButton = document.getElementById('btn-next');
-    const contactDetails = document.getElementById('contact-details-container');
+    const nextDetailsButton = document.getElementById('btn-nextt');
+    const prevButton = document.getElementById('btn-prev-third');
+    const submitButton = document.getElementById('btn-submit')
 
-    // functions of each buttons
+    // functions of each buttons for form navigation
     nextPageButton.addEventListener('click', function(){
         // when the first next button is clicked, next page displays 
         signUpContainer.style.display = 'none';
-        nameDetails.style.display = 'block';
+        signUpCon.style.display = 'block';
     });
 
     prevPageButton.addEventListener('click', function(){
-        nameDetails.style.display = 'none';
+        signUpCon.style.display = 'none';
         signUpContainer.style.display = 'block';
 
     });
 
     nextDetailsButton.addEventListener('click', function(){
-        signUpContainer.style.display = 'none';
-        contactDetails.style.display = 'block';
+        signUpCon.style.display = 'none';
+        signUpContain.style.display = 'block';
+        
     });
+    prevButton.addEventListener('click', function(){
+        signUpContain.style.display = 'none';
+        signUpCon.style.display = 'block';
+    });
+
+    submitButton.addEventListener('click', function(){
+        signUpContain.style.display = 'none';
+        signUpContainer.style.display = 'block';
+    });
+
 
 
 });
